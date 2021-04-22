979a49311ee1bfa61782b19cfc01e1638b060df8
DenysLopatka <qa.denys.lopatka@gmail.com>
2021-04-22 15:38:31 +0300

    Another Tests for Validators

--- PizzaStore.Tests/PizzaStore.Tests.cs
+++ PizzaStore.Tests/PizzaStore.Tests.cs
@@ -10,13 +10,43 @@ namespace PizzaStore.Tests
     public class Tests
     {        
         [Test]
-        public void Test1()
+        public void PositiveTestForNameValidator()
         {            
             var validator = new UserValidator();
 
-            var result = validator.IsNameValid("Nice01");            
+            var result = validator.IsNameValid("Nice 01");            
             
             Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void NegativeTestForNameValidator()
+        {
+            var validator = new UserValidator();
+
+            var result = validator.IsNameValid("Nice01 ###");
+
+            Assert.AreEqual(false, result);
+        }
+
+        [Test]
+        public void PositiveTestForAmountValidatior()
+        {
+            var validator = new UserValidator();
+
+            var result = validator.IsAmountValid(40);
+
+            Assert.AreEqual(true, result);
+        }
+
+        [Test]
+        public void NegativeTestForAmountValidatior()
+        {
+            var validator = new UserValidator();
+
+            var result = validator.IsAmountValid(-20);
+
+            Assert.AreEqual(false, result);
         }
     }
 }
