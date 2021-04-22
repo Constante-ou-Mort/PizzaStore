16204a34090aa9f9d654ac59723ab7e7159a73b1
Constante-ou-Mort <gioeffect@gmail.com>
2021-04-22 13:40:39 +0300

    Minor fix

--- PizzaStore/Menu.cs
+++ PizzaStore/Menu.cs
@@ -15,14 +15,16 @@ namespace PizzaStore
             _user = user;
         }
 
-        public void SelectOption()
+        public void MakeOrder()
         {
             Console.WriteLine($"Choose any pizza: (1){PizzaType.California} (8$) , (2){PizzaType.Detroit}(10$), (3){PizzaType.Neapolitan}(12$)");
             var pizzaType = Console.ReadLine();
 
             var pizza = _pizzaService.ChoosePizza(pizzaType);
             _pizzaService.PayForPizza(_user);
-            _pizzaService.CreatePizza(pizza);
+            var createdPizza = _pizzaService.CreatePizza(pizza);
+
+            Console.WriteLine($"{_user.Name}, please, take your {createdPizza.Name} pizza.");
         }
     }
 }
