d01737db7c6ec9db3fc1e4548645a5be2a71b6d2
Sergei Pianykh <pyanykhs@gmail.com>
2021-04-22 17:29:53 +0300

    added UserServiceTests

--- PizzaStore.Test/UserServiceTest.cs
+++ PizzaStore.Test/UserServiceTest.cs
@@ -2,6 +2,8 @@ using Xunit;
 using PizzaStore.Validators;
 using PizzaStore.Services;
 using PizzaStore.Models;
+using System;
+
 namespace PizzaStore.Test
 {
     public class UserServiceTest
@@ -17,6 +19,34 @@ namespace PizzaStore.Test
 
             // Assert
             Assert.NotNull(user);                        
+        }
+
+        [Fact]
+        public void CreateUser_CorrectExceptionMessageWithValidNameAndNotValidAmount()
+        {
+            // Arrange
+            var userService = new UserService(new UserValidator());
+            var expectedMessage = "-1 is invalid.";
+
+            // Act
+            var exception = Assert.Throws<ArgumentException>(() => userService.CreateUser("Lenid", -1));
+
+            // Assert
+            Assert.Equal(expectedMessage, exception.Message);
+        }
+
+        [Fact]
+        public void CreateUser_CorrectExceptionMessageWithNotValidNameAndValidAmount()
+        {
+            // Arrange
+            var userService = new UserService(new UserValidator());
+            var expectedMessage = "Вергилий is invalid.";
+
+            // Act
+            var exception = Assert.Throws<ArgumentException>(() => userService.CreateUser("Вергилий", 100));
+
+            // Assert
+            Assert.Equal(expectedMessage, exception.Message);
         }
     }
 }
