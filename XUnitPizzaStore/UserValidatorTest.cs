80fb7d693c25ac14a7855e57f390f38835b2dac5
Sergei Pianykh <pyanykhs@gmail.com>
2021-04-22 15:19:45 +0300

    added test for UserValidator

---
+++ XUnitPizzaStore/UserValidatorTest.cs
@@ -0,0 +1,36 @@
+﻿using PizzaStore.Validators;
+using Xunit;
+
+namespace PizzaStore.Test
+{
+    public class UserValidatorTest
+    {    
+        [Fact]
+        public void IsNameValid_LatinAndNumbersShoudValidate()
+        {
+            // Arrange
+            var userValidator = new UserValidator();
+            bool expected = true;
+
+            // Act
+            bool actual = userValidator.IsNameValid("Petya123");
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void IsNameValid_KyrrilicShoudNotValidate()
+        {
+            // Arrange
+            var userValidator = new UserValidator();
+            bool expected = false;
+
+            // Act
+            bool actual = userValidator.IsNameValid("Серега");
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
