c9b2d20d0261c48564d8b3832464f029abdf3aa2
DenysLopatka <qa.denys.lopatka@gmail.com>
2021-04-30 19:13:05 +0300

    final

---
+++ PizzaStore.Tests/PizzaServiceTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System;
+using PizzaStore;
+using PizzaStore.Validators;
+using PizzaStore.Models;
+using PizzaStore.Services;
+
+namespace PizzaStore.Tests
+{
+    class PizzaServiceTests
+    {
+        [Test]
+        public void CaliforniaPizzaTest()
+        {
+            var validator = new PizzaValidator();
+            var pizzaToBake = new PizzaService(validator).ChoosePizza("0");
+
+            Pizza result = new Pizza { Price = 8, Name = nameof(PizzaType.California) };            
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(result.Name, pizzaToBake.Name);
+                Assert.AreEqual(result.Price, pizzaToBake.Price);
+            });
+        }
+
+        [Test]
+        public void DetroitPizzaTest()
+        {
+            var validator = new PizzaValidator();
+            var pizzaToBake = new PizzaService(validator).ChoosePizza("1");
+
+            Pizza result = new Pizza { Price = 10, Name = nameof(PizzaType.Detroit) };
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(result.Name, pizzaToBake.Name);
+                Assert.AreEqual(result.Price, pizzaToBake.Price);
+            });
+        }
+
+        [Test]
+        public void NeapolitanPizzaTest()
+        {
+            var validator = new PizzaValidator();
+            var pizzaToBake = new PizzaService(validator).ChoosePizza("2");
+
+            Pizza result = new Pizza { Price = 12, Name = nameof(PizzaType.Neapolitan) };
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(result.Name, pizzaToBake.Name);
+                Assert.AreEqual(result.Price, pizzaToBake.Price);
+            });
+        }
+
+        [Test]
+        public void PayForPizzaTest (string name = "vasiliy", double amount = 50)
+        {
+            var validator = new PizzaValidator();
+            var pizzaService = new PizzaService(validator);
+            var user = new User(name, amount);
+
+            pizzaService.ChoosePizza("0");
+            var result = user.Amount - 8;
+
+            pizzaService.PayForPizza(user);
+
+            Assert.AreEqual(result, user.Amount);
+        }
+                
+        [Test]
+        public void TryToMakeDetroit()
+        {
+            var pizzaService = new PizzaService(new PizzaValidator());
+            var choose = pizzaService.ChoosePizza("Detroit");
+            var pizza = PizzaIngredientsService.GetIngredientsByPizzaType(choose.Type);
+
+            List<string> ingridients = new List<string>() { "White sugar", "Olive oil", "Bread flour", "Kosher salt", "Red onion", "Garlic", "Mushrooms" };
+
+            Assert.AreEqual(ingridients, pizza);
+        }
+    }
+}
